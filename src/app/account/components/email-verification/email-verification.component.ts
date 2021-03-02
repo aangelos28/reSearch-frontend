8d73359420e7116cb94274f0254c0c97836ab73d
Angelos Angelopoulos <aangelos28@gmail.com>
2021-03-02 18:31:27 -0500

    Add work tracking to resend email confirmation

--- src/app/account/components/email-verification/email-verification.component.ts
+++ src/app/account/components/email-verification/email-verification.component.ts
@@ -5,6 +5,7 @@ import {Router} from '@angular/router';
 import {AccountService} from '../../services/account/account.service';
 import {MatDialog} from '@angular/material/dialog';
 import {InfoDialogComponent} from '../../../shared/components/info-dialog/info-dialog.component';
+import {WorkTrackerService} from '../../../shared/services/work-tracker/work-tracker.service';
 
 @Component({
   selector: 'app-email-verification',
@@ -16,7 +17,7 @@ export class EmailVerificationComponent implements OnInit {
   public email: Promise<string> = this.accountService.getUserEmailAsync();
 
   constructor(private accountService: AccountService, private authService: AuthService, private router: Router,
-              private snackBar: MatSnackBar, private dialog: MatDialog) {
+              private workTracker: WorkTrackerService, private snackBar: MatSnackBar, private dialog: MatDialog) {
   }
 
   public ngOnInit(): void {
@@ -28,16 +29,19 @@ export class EmailVerificationComponent implements OnInit {
   }
 
   public resendConfirmationEmail(): void {
-    this.accountService.user.sendEmailVerification().then(() =>
+    this.workTracker.startWork();
+    this.accountService.user.sendEmailVerification().then(() => {
+      this.workTracker.finishWork();
       this.dialog.open(InfoDialogComponent, {
         data: {
           title: 'Confirmation Email Resent',
           text: 'Confirmation email resent. Please check your inbox.'
         }
-      })
-    ).catch(err =>
-      this.dialog.open(InfoDialogComponent, {data: {title: 'Error', text: `Failed to resend confirmation email.\n${err}`}})
-    );
+      });
+    }).catch(err => {
+      this.workTracker.finishWork();
+      this.dialog.open(InfoDialogComponent, {data: {title: 'Error', text: `Failed to resend confirmation email.\n${err}`}});
+    });
   }
 
   public backToLogin(): void {
